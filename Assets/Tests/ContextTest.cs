76b745c94124e661a3e2d1fea3c209aa8cffbeef
Segelzwerg <marcel.haas@hhu.de>
2021-12-23 15:33:58 +0100

    ADD: very basic tests

---
+++ Assets/Tests/ContextTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ContextTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void ContextTestSimplePasses()
+    {
+        Assert.IsTrue(true);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator ContextTestWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+        Assert.IsTrue(true);
+    }
+}
