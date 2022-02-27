df7382689ed546484dca141fb29db7e2a1823a6b
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-02-27 23:04:11 +0100

    ADD: test for login
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

---
+++ Assets/Tests/UI/Lobby/TestLogin.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Connections;
+using NUnit.Framework;
+using Session;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests.UI.Lobby
+{
+    public class TestLogin
+    {
+        private string _json;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            SceneManager.LoadScene("LobbyScene");
+            Dictionary<string, string> userDict = new Dictionary<string, string>
+            {
+                { "username", "marcel" }, { "password", "pwd" }
+            };
+            _json = "{ \"username\":\"marcel\", \"password\":\"pwd\"}";
+        }
+
+
+        [UnityTest]
+        public IEnumerator TestLoginSuccess()
+        {
+            GameObject serverConnection = GameObject.Find("ServerConnection");
+            ServerConnection.SendJsonString("http://localhost:9001/user/create", _json, null);
+            yield return new WaitForSeconds(2);
+
+            Login.Login login = serverConnection.GetComponent<Login.Login>();
+            login.username.text = "marcel";
+            login.password.text = "pwd";
+            yield return null;
+            login.SendRequest();
+            yield return new WaitForSeconds(2);
+            Assert.IsNotNull(SessionManager.GetToken());
+        }
+    }
+}
